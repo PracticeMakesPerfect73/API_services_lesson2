733ec497d6e4bdb4f9216ece60ec7ed935124ce1
Alexey <practicemakesperfect73@gmail.com>
2025-02-17 00:41:17 +0900

    Упаковал def main и переместил перехват исключений

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 
+def make_request(api_url, params):
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
 def is_shorten_link(token, link):
     api_url = 'https://api.vk.ru/method/utils.getLinkStats'
     key = urlparse(link).path.strip('/')
@@ -11,17 +17,8 @@ def is_shorten_link(token, link):
               "key": key,
               "v": "5.199"
               }
-    try:
-        response = requests.get(api_url, params=params)
-        response.raise_for_status()
-        answer = response.json()
-        return "response" in answer
-    except requests.exceptions.HTTPError as e:
-        return f"Ошибка запроса: {e}"
-    except ValueError as e:
-        return f"{e}"
-    except KeyError:
-        return "Ошибка: неожиданный формат ответа от API."
+    answer = make_request(api_url, params)
+    return "response" in answer
 
 
 def shorten_link(token, link):
@@ -30,18 +27,9 @@ def shorten_link(token, link):
               "url": link,
               "v": "5.199"
               }
-    try:
-        response = requests.get(api_url, params=params)
-        response.raise_for_status()
-        answer = response.json()
-        short_link = answer["response"]["short_url"]
-        return f"Сокращенная ссылка: {short_link}"
-    except requests.exceptions.HTTPError as e:
-        return f"Ошибка запроса: {e}"
-    except ValueError as e:
-        return f"{e}"
-    except KeyError:
-        return "Ошибка: неожиданный формат ответа от API."
+    answer = make_request(api_url, params)
+    short_link = answer["response"]["short_url"]
+    return short_link
 
 
 def count_clicks(token, link):
@@ -51,32 +39,28 @@ def count_clicks(token, link):
               "key": key,
               "v": "5.199"
               }
+    answer = make_request(api_url, params)
+    link_stats = answer["response"]["stats"][0]["views"]
+    return link_stats
+
+
+def main():
+    load_dotenv()
     vk_token = os.environ["VK_TOKEN"]
+    user_input = input("Введите ссылку: ")
     try:
-        response = requests.get(api_url, params=params)
-        response.raise_for_status()
-        answer = response.json()
-        link_stats = answer["response"]["stats"][0]["views"]
-        return f"Количество кликов: {link_stats}"
+        if is_shorten_link(vk_token, user_input):
+            clicks = count_clicks(vk_token, user_input)
+            print(f"Количество кликов: {clicks}")
+        else:
+            short_link = shorten_link(vk_token, user_input)
+            print(f"Сокращенная ссылка: {short_link}")
     except requests.exceptions.HTTPError as e:
-        return f"Ошибка запроса: {e}"
+        print(f"Ошибка запроса: {e}")
     except ValueError as e:
-        return f"{e}"
+        print(f"{e}")
     except KeyError:
-        return "Ошибка: неожиданный формат ответа от API."
-
-
-def main(token, link):
-    if is_shorten_link(token, link):
-        return count_clicks(token, link)
-    else:
-        return shorten_link(token, link)
-
+        print("Ошибка: неожиданный формат ответа от API.")
 
 if __name__ == "__main__":
-    load_dotenv()
-    vk_token = os.getenv("VK_TOKEN")
-    user_input = input("Введите ссылку: ")
-    result = main(vk_token, user_input)
-    print(result)
-    
+    main()
